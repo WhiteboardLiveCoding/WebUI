b686cc8ebbdf623731494f7038cc861896060f65
SuNaden <stollarf@gmail.com>
2017-11-02 23:47:09 +0100

    Add some html elements and render code

--- application.py
+++ application.py
@@ -2,54 +2,38 @@ import os
 import requests
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
-UPLOAD_FOLDER = 'uploads/'
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = 'uploads/'
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('base.html', template='index.html')
+    if request.method == 'POST':
+        file = request.files['file']
+        if 'file' not in request.files or file.filename == '':
+            return render_template('base.html', template='index.html')
+        elif file and allowed_file(file.filename):
+            # Save file to file system
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            filesFound = {'file':open(app.config['UPLOAD_FOLDER'] + filename, 'rb')}
 
-@app.route('/code')
-def code():
-    return render_template('base.html', template='code.html')
+            r = requests.post('http://0.0.0.0:5000/api/upload_image', files=filesFound)
+            r = r.json()
+            # Remove file immediately after we submit it to the other endpoint
+            os.remove(app.config['UPLOAD_FOLDER'] + filename)
+            # Yeah, request parsing is weird
+            unfixed = r[1]
+            fixed = r[3]
+            result = r[5]
+            error = r[7]
+
+            return render_template('base.html', template='code.html', fixed=fixed)
+    else:
+        return render_template('base.html', template='index.html')
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/data', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            filesFound = {'file':open(app.config['UPLOAD_FOLDER'] + filename, 'rb')}
-            r = requests.post('http://0.0.0.0:5000/api/upload_image', files=filesFound)
-            r = r.json()
-            unfixed = r[1]
-            fixed = r[3]
-            print(unfixed + '\n \n' + fixed)
-            return 'Okayt'
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
