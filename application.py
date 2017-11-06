386e70d2de74f6b5b292fc55f1ad8ef67d5e5850
Martin Zlocha <mz4315@ic.ac.uk>
2017-11-06 20:04:40 +0000

    Remove unneeded saving and refactored.

--- application.py
+++ application.py
@@ -1,59 +1,28 @@
-import os
 import requests
-from flask import Flask, render_template, request, redirect, url_for
-from werkzeug.utils import secure_filename
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads/'
-app.config['IMAGE_PROCESSOR'] = 'http://0.0.0.0:5000/api/upload_image'
+app.config['IMAGE_PROCESSOR'] = 'http://whiteboardlivecoding-ocr.azurewebsites.net/api/upload_image'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        create_directory()
         file = request.files['file']
+
         if 'file' not in request.files or file.filename == '':
             return render_template('base.html', template='index.html')
-        elif file and allowed_file(file.filename):
-            # Save file to file system
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            open_file = open(app.config['UPLOAD_FOLDER'] + filename, 'rb')
-
-            # Build request and close the file
-            r = requests.post(app.config['IMAGE_PROCESSOR'],
-                              files={'file':open_file.read()})
+        elif file:
+            r = requests.post(app.config['IMAGE_PROCESSOR'], files={'file': file.read()})
             r = r.json()
-            open_file.close()
 
-            # Yeah, request parsing is weird
-            unfixed = r[1]
-            fixed = r[3]
-            result = r[5]
-            error = r[7]
+            fixed = r.get('fixed')
+            result = r.get('result')
+            error = r.get('error')
 
-            rendered = render_template('base.html',
-                                       template='code.html',
-                                       fixed=fixed,
-                                       result=result,
-                                       error=error)
+            # Use this when resubmitting code to run
+            key = r.get('key')
 
-            # Remove file after everything else is completeds
-            os.remove(app.config['UPLOAD_FOLDER'] + filename)
-
-            return rendered
+            return render_template('base.html', template='code.html', fixed=fixed, result=result, error=error)
     else:
         return render_template('base.html', template='index.html')
-
-
-def create_directory():
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
